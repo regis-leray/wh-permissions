f0c49c38d0d55de276dae162ea061c21df4cb4f0
Oleg Korolenko <oleg.korolenko@williamhill.co.uk>
2022-05-27 10:36:13 +0000

    [UNITY-14189] Added a bit of logging in the main loop

--- src/main/scala/com/williamhill/permission/Main.scala
+++ src/main/scala/com/williamhill/permission/Main.scala
@@ -1,22 +1,24 @@
 package com.williamhill.permission
 
 import com.github.mlangc.slf4zio.api.logging as Log
-import com.williamhill.permission.application.{Env, Kamon}
+import com.williamhill.permission.application.Env
 import zio.{App, ExitCode, URIO, ZEnv}
 
 object Main extends App {
-  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
-    Kamon.asResource
-      .use_(
-        Log.warnIO("commence consumption of self-exclusion events") *>
-          Processor.run
-            .mapError(appError => appError.toThrowable)
-            .tapBoth(
-              Log.errorIO("stopped consumption of self-exclusion events, faiiled with error", _),
-              _ => Log.warnIO("stopped consumption of self-exclusion events"),
-            )
-            .as(ExitCode.success),
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
+    println(s"App starting ......")
+//    Kamon.asResource
+//      .use_(
+//    Log.warnIO("START consuming input events") *>
+    Processor.run
+      .mapError(appError => appError.toThrowable)
+      .tapBoth(
+        Log.errorIO("STOP consuming input events => failed with error", _),
+        _ => Log.warnIO("STOP consuming input events"),
       )
+      .as(ExitCode.success)
+//      )
       .provideSomeLayer[ZEnv](Env.layer)
       .orDie
+  }
 }
